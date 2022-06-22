6ad5d0f8e92e1edd3f98bdd878cc2c2b6f41fd88
Guido Queiroz <Takzzg@hotmail.com>
2022-06-22 16:50:42 -0300

    extra navigate

--- client/src/components/Auth/Login.jsx
+++ client/src/components/Auth/Login.jsx
@@ -57,7 +57,7 @@ export default function Login() {
         } catch (error) {
             console.log(error)
         }
-        Navigate("/")
+        // Navigate("/")
     }
 
     useEffect(() => {
