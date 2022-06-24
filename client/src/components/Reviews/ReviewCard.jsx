8e715aace91d194bfad346b29b1da207e836a78c
Guido Queiroz <Takzzg@hotmail.com>
2022-06-24 15:54:51 -0300

    review card basic layout

--- client/src/components/Reviews/ReviewCard.jsx
+++ client/src/components/Reviews/ReviewCard.jsx
@@ -1,12 +1,21 @@
 import React from "react"
+import { StyledReviewCard } from "./Reviews.styled"
 
 const ReviewCard = ({ review }) => {
     return (
-        <div>
-            {review.title}
-            {review.comment}
-            {review.userId.name} {"->"} {review.productId.name}
-        </div>
+        <StyledReviewCard>
+            <span className="header">
+                <span className="title">{review.title}</span>
+                <span className="score">{review.score}/5</span>
+            </span>
+            <span className="comment">{review.comment}</span>
+            <span className="info">
+                <span className="author">
+                    {review.userId.name} {"->"} {review.productId.name}
+                </span>
+                <span className="date">{review.date}</span>
+            </span>
+        </StyledReviewCard>
     )
 }
 
--- client/src/components/Reviews/Reviews.styled.js
+++ client/src/components/Reviews/Reviews.styled.js
@@ -7,3 +7,15 @@ export const StyledUserReviews = styled.div`
     gap: 1rem;
     align-items: center;
 `
+export const StyledReviewCard = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 1rem;
+    gap: 1rem;
+
+    .header,
+    .info {
+        display: flex;
+        justify-content: space-between;
+    }
+`
