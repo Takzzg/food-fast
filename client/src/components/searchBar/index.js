8a5cc1e8e1cd5c696eea360dbe419fe75c4cf9e6
Guido Queiroz <Takzzg@hotmail.com>
2022-06-23 21:50:29 -0300

    searchbar index fix mariano

--- client/src/components/searchBar/index.js
+++ client/src/components/searchBar/index.js
@@ -1,127 +1,82 @@
-import React, { useEffect, useState } from "react";
-import { AiOutlineSearch } from "react-icons/ai";
-import { GlobalContainer, SearchIcon } from "./searchElements";
-import { useDispatch, useSelector } from "react-redux";
-import { searchProductSync } from "../../redux/actions/sync";
-import { searchProductAsync } from "../../redux/actions/async";
-import Autosuggest from "react-autosuggest";
-import "./autoStyles.scss"
-import axios from "axios";
+import React, { useEffect, useState } from "react"
 
+import { GlobalContainer, SearchIcon, SearchInput } from "./searchElements"
+import { useDispatch, useSelector } from "react-redux"
+import { searchProduct } from "../../redux/actions/async"
+import { BsFillMicFill } from "react-icons/bs"
+import { BsFillMicMuteFill } from "react-icons/bs"
+import style from "./style/mic.module.scss"
 
+const SpeechRecognition =
+    window.SpeechRecognition || window.webkitSpeechRecognition
+const mic = new SpeechRecognition()
 
-const SpeechRecognition=window.SpeechRecognition || window.webkitSpeechRecognition
-const mic=new SpeechRecognition()
+mic.continuous = true
+mic.interimResults = true
+mic.lang = "es-ES"
 
 export default function SearchBar() {
-  const [input, setInput] = useState("");
-  const dispatch = useDispatch();
-  const theme = useSelector((state) => state.theme.selectedTheme);
+    const [input, setInput] = useState("")
+    const dispatch = useDispatch()
+    const theme = useSelector((state) => state.theme.selectedTheme)
+    const [listen, setListen] = useState(false)
 
+    const handleListen = () => {
+        if (listen) {
+            mic.start()
 
-  // ==================================
-  const productsF = useSelector(state=> state.main.products.filtered);
-  const allProducts = useSelector(state=> state.main.products.all)
-  const [products, setProducts] = useState(productsF ? productsF:[]); 
-
-  const renderSuggestion = (suggestion)=> (
-    <div onClick={()=> handleSelect(suggestion.name)}>
-        {`${suggestion.name}`}
-    </div>); 
-
-    const onSuggestionsClearRequested = ()=>setProducts(allProducts); 
-
-    const onSuggestionsFetchRequested = ({value}) => setProducts(filterProducts(value)); 
-    const getSuggestionValue = (suggestion) => `${suggestion.name}`
-    
-    const handleSelect = (product)=> {
-      setInput("");
-      dispatch(searchProductAsync(product))
-    } 
-
-    const filterProducts=(value)=>{
-      const inputValue = value.trim().toLowerCase();
-      const inputLength = inputValue.length;
-    
-      let filteredProducts = allProducts.filter(product=>{
-        let completeName = product.name;
-        if(completeName.toLowerCase()
-        .normalize("NFD")
-        .replace(/[\u0300-\u036f]/g, "")
-        .includes(inputValue)){
-          return product;
+            mic.onend = () => {
+                console.log("continue...")
+                mic.start()
+            }
+        } else {
+            mic.stop()
+            mic.onend = () => {
+                console.log("micrófono en stop")
+            }
+            setInput("")
         }
+        mic.onstart = () => {
+            console.log("Micrófono encendido...")
+        }
+        mic.onresult = (event) => {
+            const transcript = Array.from(event.results)
+                .map((result) => result[0])
+                .map((result) => result.transcript)
+                .join("")
+            console.log(transcript)
+            setInput(transcript)
+            mic.onerror = (event) => console.log(event.error)
+        }
+    }
 
-        mic.onresult=(event)=>{
-            const transcript=Array.from(event.results)
-            .map((result)=>result[0])
-           .map((result)=>result.transcript).join("");
-           console.log(transcript)
-           setInput(transcript)
-           mic.onerror=(event)=>console.log(event.error)
-       }
-     }
+    useEffect(() => {
+        handleListen()
+    }, [listen])
 
-     
-
-     React.useEffect(()=>{
-        handleListen();
-      },[listen])
-
-      const handleVoiceClick=()=>{
+    const handleVoiceClick = () => {
         dispatch(searchProduct(input))
-        setListen(prevState=>!prevState)
-       
-        
-      }
+        setListen((prevState) => !prevState)
+    }
     const handleChange = (e) => {
         setInput(e.target.value)
         dispatch(searchProduct(e.target.value))
-        
-
-      });
-      return inputLength===0 ? [] : filteredProducts;
-
     }
 
-    const getData = ()=> {
-      axios.get("http://localhost:3001/api/v1/products").then(response=>{
-        setProducts(response.data);
-      })
+    const handleSubmit = () => {
+        dispatch(searchProduct(input))
     }
-  // ================================
-  const handleChange = (e) => {
-    setInput(e.target.value);
-    dispatch(searchProductSync(e.target.value));
-  };
-
-  const handleClean = ()=> {
-    dispatch(searchProductSync(""))
-  }
-  const inputProps = {
-    placeholder: "Product name",
-    value: input,
-    onChange: handleChange,
-  };
-
-  useEffect(()=> {
-    getData();
-  }, [])
-  return (
-    <GlobalContainer>
-      <Autosuggest
-        suggestions={products}
-        onSuggestionsFetchRequested={onSuggestionsFetchRequested}
-        onSuggestionsClearRequested={onSuggestionsClearRequested}
-        getSuggestionValue={getSuggestionValue}
-        renderSuggestion={renderSuggestion}
-        inputProps={inputProps}
-        onSuggestionSelected={()=> setInput("")}
-      />
-      <button onClick={handleClean}>Clean</button>
-      <SearchIcon theme={theme} onClick={handleSelect}>
-        <AiOutlineSearch />
-      </SearchIcon>
-    </GlobalContainer>
-  );
+    return (
+        <GlobalContainer>
+            <SearchInput
+                name="searchBar"
+                value={input}
+                onChange={handleChange}
+                placeholder={listen ? "Listening..." : "Search..."}
+            />
+            <button className={style.mic} onClick={handleVoiceClick}>
+                {listen ? <BsFillMicFill /> : <BsFillMicMuteFill />}
+            </button>
+        </GlobalContainer>
+    )
 }
